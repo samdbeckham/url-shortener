98839366f11373ccb06a563b2f17336edae4076a
samdbeckham <sam@beckham.io>
2025-08-04 14:24:54 +0100

    Started adding dummy data to the db

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import sqlite3
 from typing import Union
 from fastapi import FastAPI
 
 tld = "https://url.beckham.io"
 app = FastAPI()
 
-@app.get("/{alias}")
-def read_alias(alias: str):
-    # TODO: Fetch the URL from the database
+def get_db():
+    con = sqlite3.connect("tmp.db")
+    cur = con.cursor()
+    return (con, cur)
+
+
+def initialize_db():
+    (con, cur) = get_db()
+    cur.execute("CREATE TABLE urls(alias, url)")
+    cur.execute("INSERT INTO urls VALUES ('test', 'https://google.com')")
+    con.commit()
+
+@app.get("/test")
+def test():
+    initialize_db()
+    (con, cur) = get_db();
+    res = cur.execute("SELECT name FROM sqlite_master")
+    return {"test": "success", "alias": res.fetchone()}
+
+@app.get("/{alias_id}")
+def read_alias(alias_id: str):
+    (con, cur) = get_db()
+    res = cur.execute("SELECT alias, url FROM urls")
+    (alias, url) = res.fetchone()
+    
     # TODO: 302 to the actual URL
-    return {"alias": alias, "url": "https://google.com" }
+
+    return {"alias": alias, "url": url }
 
 @app.put("/api/shorten")
 def shorten_url(alias: str, url: str):

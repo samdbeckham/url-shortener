815ebafe45f90e3aead5d5a3e8e3c6ac81822a39
samdbeckham <sam@beckham.io>
2025-08-05 22:46:39 +0100

    Removes unused seed function

--- functions/seed_db.py
+++
@@ -1,8 +0,0 @@
-from .get_db import get_db
-
-def seed_db():
-    (con, cur) = get_db()
-    cur.execute("CREATE TABLE urls(alias, url)")
-    cur.execute("INSERT INTO urls VALUES ('testalias1', 'https://google.com')")
-    cur.execute("INSERT INTO urls VALUES ('testalias2', 'https://netflix.com')")
-    con.commit()

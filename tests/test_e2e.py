cb1be17ec865a856a538e025100c37ca7ab39bc8
samdbeckham <sam@beckham.io>
2025-08-05 14:16:30 +0100

    Properly tears down tests

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,66 +1,73 @@
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+from functions.teardown import teardown
 
 client = TestClient(app, follow_redirects=False)
 ALIAS = "heisenberg"
 URL = "https://www.savewalterwhite.com"
 
-def test_health():
-    response = client.get("/api/health")
-    assert response.status_code == 200
-    assert response.json() == { "status": "healthy" }
+class TestE2E:
+    @classmethod
+    def teardown_class(cls):
+        teardown()
 
-def test_no_redirect():
-    response = client.get(f"/{ALIAS}")
-    assert response.status_code == 404
-    assert response.json() == { "detail": "Alias not found" }
+    def test_health(self):
+        response = client.get("/api/health")
+        assert response.status_code == 200
+        assert response.json() == { "status": "healthy" }
 
-def test_not_found():
-    response = client.get("/api/read", params={"alias": ALIAS})
-    assert response.status_code == 404
-    assert response.json() == { "detail": "Alias not found" }
+    def test_no_redirect(self):
+        response = client.get(f"/{ALIAS}")
+        assert response.status_code == 404
+        assert response.json() == { "detail": "Alias not found" }
 
-def test_shorten():
-    response = client.put("/api/shorten", params={"alias": ALIAS, "url": URL})
-    assert response.status_code == 200
-    assert response.json()['short_url'] == f"https://url.beckham.io/{ALIAS}"
-    assert response.json()['alias'] == ALIAS
-    assert response.json()['url'] == URL
+    def test_not_found(self):
+        response = client.get("/api/read", params={"alias": ALIAS})
+        assert response.status_code == 404
+        assert response.json() == { "detail": "Alias not found" }
 
-def test_is_found():
-    response = client.get("/api/read", params={"alias": ALIAS})
-    assert response.status_code == 200
-    assert response.json()['alias'] == ALIAS
-    assert response.json()['url'] == URL
+    def test_shorten(self):
+        response = client.put("/api/shorten", params={"alias": ALIAS, "url": URL})
+        assert response.status_code == 200
+        assert response.json()['short_url'] == f"https://url.beckham.io/{ALIAS}"
+        assert response.json()['alias'] == ALIAS
+        assert response.json()['url'] == URL
 
-def test_redirect():
-    response = client.get(f"{ALIAS}")
-    assert response.status_code == 302
-    assert response.headers["location"] == URL
+    def test_is_found(self):
+        response = client.get("/api/read", params={"alias": ALIAS})
+        assert response.status_code == 200
+        assert response.json()['alias'] == ALIAS
+        assert response.json()['url'] == URL
 
-def test_shorten_override():
-    response = client.put("/api/shorten", params={"alias": ALIAS, "url": URL})
-    assert response.status_code == 409
-    assert response.json() == { "detail": "Alias already exists" }
+    def test_redirect(self):
+        response = client.get(f"{ALIAS}")
+        assert response.status_code == 302
+        assert response.headers["location"] == URL
 
-def test_delete():
-    response = client.delete("/api/delete", params={"alias": ALIAS})
-    assert response.status_code == 200
-    assert response.json() == { "detail": "Alias deleted" }
+    def test_shorten_override(self):
+        response = client.put("/api/shorten", params={"alias": ALIAS, "url": URL})
+        assert response.status_code == 409
+        assert response.json() == { "detail": "Alias already exists" }
 
-def test_not_found_again():
-    response = client.get("/api/read", params={"alias": ALIAS})
-    assert response.status_code == 404
-    assert response.json() == { "detail": "Alias not found" }
+    def test_delete(self):
+        response = client.delete("/api/delete", params={"alias": ALIAS})
+        assert response.status_code == 200
+        assert response.json() == { "detail": "Alias deleted" }
 
-def test_no_redirect_again():
-    response = client.get(f"/{ALIAS}")
-    assert response.status_code == 404
-    assert response.json() == { "detail": "Alias not found" }
+    def test_not_found_again(self):
+        response = client.get("/api/read", params={"alias": ALIAS})
+        assert response.status_code == 404
+        assert response.json() == { "detail": "Alias not found" }
 
-def test_double_delete():
-    response = client.delete("/api/delete", params={"alias": ALIAS})
-    assert response.status_code == 404
-    assert response.json() == { "detail": "Alias not found" }
+    def test_no_redirect_again(self):
+        response = client.get(f"/{ALIAS}")
+        assert response.status_code == 404
+        assert response.json() == { "detail": "Alias not found" }
+
+    def test_double_delete(self):
+        response = client.delete("/api/delete", params={"alias": ALIAS})
+        assert response.status_code == 404
+        assert response.json() == { "detail": "Alias not found" }
+
 

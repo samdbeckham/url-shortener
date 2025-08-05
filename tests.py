059820a72bffa4a7d7c723c805f967a89b535e66
samdbeckham <sam@beckham.io>
2025-08-05 08:29:36 +0100

    Tests the url shortener endpoint

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ from main import app
 
 client = TestClient(app)
 ALIAS = "heisenberg"
+URL = "https://www.savewalterwhite.com"
 
 class Tests(unittest.TestCase):
     # get_db
@@ -31,25 +32,46 @@ class Tests(unittest.TestCase):
         self.assertEqual(response.json(), { "detail": "Alias not found" })
 
     def test_shorten(self):
-        pass
-    def test_is_found(self):
-        pass
-    def test_redirect(self):
-        pass
-    def test_shorten_override(self):
-        pass
-    def test_delete(self):
-        pass
-    def test_still_not_found(self):
-        pass
-    def test_still_no_redirect(self):
-        pass
-    def test_double_delete(self):
-        pass
+        response = client.put("/api/shorten", params={"alias": ALIAS, "url": URL})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['short_url'], "https://url.beckham.io/heisenberg")
+        self.assertEqual(response.json()['alias'], "heisenberg")
+        self.assertEqual(response.json()['url'], "https://www.savewalterwhite.com")
 
-    def test_test(self):
-        test_value = 42
-        self.assertEqual(test_value, 42)
+    def test_is_found(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_redirect(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_shorten_override(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_delete(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_still_not_found(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_still_no_redirect(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
+        
+    def test_double_delete(self):
+        response = client.get("/api/read", params={"alias_id": ALIAS})
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), { "detail": "Alias not found" })
 
 if __name__ == "__main__":
     unittest.main()

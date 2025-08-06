a41e2736df945a66cf7bfbb54635acf9801b64f7
samdbeckham <sam@beckham.io>
2025-08-06 11:25:51 +0100

    Checks for collisions with random aliases

--- endpoints/shorten_endpoint.py
+++ endpoints/shorten_endpoint.py
@@ -10,11 +10,16 @@ router = APIRouter()
 
 @router.put("/shorten")
 def shorten_url(url: str, alias: str = None):
-    if alias is None:
-        alias = generate_alias()
-
-    if fetch_url_details(alias) is not None:
+    if fetch_url_details(alias) is not None and alias is not None:
         raise HTTPException(status_code=409, detail="Alias already exists")
+
+    # NOTE: We loop here to make sure the random alias isn't already taken
+    #       In most instances, this loop will only run once.
+    if alias is None:
+        while True:
+            alias = generate_alias()
+            if fetch_url_details(alias) is None:
+                break
 
     if not validators.url(url):
         raise HTTPException(status_code=400, detail="Invalid URL")

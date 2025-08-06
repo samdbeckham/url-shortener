09f1675950558b5d59e7db197eba8fe14ce1ad76
samdbeckham <sam@beckham.io>
2025-08-06 09:52:49 +0100

    Increments visits by 1 when url is visited

--- endpoints/redirect_endpoint.py
+++ endpoints/redirect_endpoint.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import RedirectResponse
 from functions.fetch_url_details import fetch_url_details
+from functions.get_db import get_db
 
 router = APIRouter()
 
@@ -10,5 +11,10 @@ def redirect_to_url(alias: str):
     data = fetch_url_details(alias)
     if data is None:
         raise HTTPException(status_code=404, detail="Alias not found")
-    url = data[1]
+
+    (_, url, visits) = data
+    (con, cur) = get_db()
+    cur.execute("UPDATE urls SET visits = ? WHERE alias = ?", (visits + 1, alias))
+    con.commit()
+
     return url
